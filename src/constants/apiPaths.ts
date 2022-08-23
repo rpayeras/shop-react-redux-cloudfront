3864297e6122f7865ac111abc3001d6f797f7809
Roberto Payeras <payerasam@gmail.com>
2022-08-23 10:23:33 +0200

    Added cart api

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,7 @@ const API_PATHS = {
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: process.env.REACT_APP_API_IMPORT,
   bff: process.env.REACT_APP_API_PRODUCTS,
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  cart: process.env.REACT_APP_API_CART,
 };
 
 export default API_PATHS;
