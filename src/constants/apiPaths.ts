58f0fd6292e6c9fbb7c020a590c8a8f06921a8b1
Roberto Payeras <payerasam@gmail.com>
2022-09-07 11:40:05 +0200

    Last changes of course

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,6 +1,6 @@
 const API_PATHS = {
   product: process.env.REACT_APP_API_PRODUCTS,
-  order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  order: process.env.REACT_APP_API_CART,
   import: process.env.REACT_APP_API_IMPORT,
   bff: process.env.REACT_APP_API_PRODUCTS,
   cart: process.env.REACT_APP_API_CART,
